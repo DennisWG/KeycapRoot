94bf77e582e74e52e8bb9110d80269c7f9cfab9d
DennisWG <DennisWG@users.noreply.github.com>
2017-03-23 08:30:14 +0100

    formatting

--- src/test/Configuration/ConfigFile.cpp
+++ src/test/Configuration/ConfigFile.cpp
@@ -23,23 +23,23 @@ TEST_CASE("Constructing a ConfigFile", "[ConfigFile]")
 {
     SECTION("Given a valid file path with valid json, constructing a ConfigFile should not throw")
     {
-        REQUIRE_NOTHROW(config::ConfigFile{ "../test.cfg" });
+        REQUIRE_NOTHROW(config::ConfigFile{"../test.cfg"});
     }
 
     SECTION("Given a valid file path with invalid json, constructing a ConfigFile should throw")
     {
-        REQUIRE_THROWS(config::ConfigFile{ "../invalid.cfg" });
+        REQUIRE_THROWS(config::ConfigFile{"../invalid.cfg"});
     }
 
     SECTION("Given an invalid file path, constructing a ConfigFile should throw")
     {
-        REQUIRE_THROWS(config::ConfigFile{ "invalid/File/path.cfg" });
+        REQUIRE_THROWS(config::ConfigFile{"invalid/File/path.cfg"});
     }
 }
 
 TEST_CASE("Testing ConfigFile::Get", "[ConfigFile]")
 {
-    config::ConfigFile cfgFile{ "../test.cfg" };
+    config::ConfigFile cfgFile{"../test.cfg"};
 
     SECTION("ConfigFile::Get must return the requested value if both the category and the value are valid")
     {
@@ -77,7 +77,7 @@ TEST_CASE("Testing ConfigFile::Get", "[ConfigFile]")
 
 TEST_CASE("Testing ConfigFile::GetOrDefault", "[ConfigFile]")
 {
-    config::ConfigFile cfgFile{ "../test.cfg" };
+    config::ConfigFile cfgFile{"../test.cfg"};
 
     SECTION("ConfigFile::GetOrDefault must return the requested value if both the category and the value are valid")
     {
@@ -97,21 +97,24 @@ TEST_CASE("Testing ConfigFile::GetOrDefault", "[ConfigFile]")
         REQUIRE(value == false);
     }
 
-    SECTION("ConfigFile::GetOrDefault must return the given default value if attempting to retreive a value from empty category")
+    SECTION("ConfigFile::GetOrDefault must return the given default value if attempting to retreive a value from empty "
+            "category")
     {
         int const defaultValue = 0;
         auto value = cfgFile.GetOrDefault<int>("Empty", "Invalid", defaultValue);
         REQUIRE(value == defaultValue);
     }
 
-    SECTION("ConfigFile::GetOrDefault must return the given default value if attempting to retreive non existant value from category")
+    SECTION("ConfigFile::GetOrDefault must return the given default value if attempting to retreive non existant value "
+            "from category")
     {
         std::string const defaultValue = "-";
         auto value = cfgFile.GetOrDefault<std::string>("Network", "Invalid", defaultValue);
         REQUIRE(value == defaultValue);
     }
 
-    SECTION("ConfigFile::GetOrDefault must return the given default value if attempting to retreive non existant value outside of category")
+    SECTION("ConfigFile::GetOrDefault must return the given default value if attempting to retreive non existant value "
+            "outside of category")
     {
         bool const defaultValue = true;
         auto value = cfgFile.GetOrDefault<bool>("", "Invalid", defaultValue);
