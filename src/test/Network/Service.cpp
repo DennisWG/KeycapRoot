554d43a283e8e4951e5b55ebc25360505c8cc47a
DennisWG <DennisWG@users.noreply.github.com>
2017-08-22 22:16:06 +0200

    moved the BaseHandler out of Service.cpp into BaseConnectionHandler.hpp and renamed it to fit the file's name

--- src/test/Network/Service.cpp
+++ src/test/Network/Service.cpp
@@ -1,3 +1,5 @@
+#include "BaseConnectionHandler.hpp"
+
 #include <Keycap/Root/Network/DataRouter.hpp>
 #include <Keycap/Root/Network/MessageHandler.hpp>
 #include <Keycap/Root/Network/Service.hpp>
@@ -8,55 +10,9 @@
 
 namespace net = Keycap::Root::Network;
 
-class BaseHandler : public std::enable_shared_from_this<BaseHandler>
+struct ClientHandler : public BaseConnectionHandler
 {
-  protected:
-    boost::asio::io_service& service_;
-    boost::asio::ip::tcp::socket socket_;
-    net::DataRouter const& router_;
-
-    std::string payload_;
-    std::array<char, 128> buffer_{0};
-
-    void Send(std::string const& msg)
-    {
-        payload_ = msg;
-
-        boost::asio::async_write(
-            socket_, boost::asio::buffer(payload_),
-            [me = shared_from_this()](const boost::system::error_code& error, std::size_t bytes_transferred){});
-    }
-
-    void Receive()
-    {
-        socket_.async_read_some(
-            boost::asio::buffer(buffer_), [me = shared_from_this()](const boost::system::error_code& error,
-                                                                    std::size_t bytesRead) {
-                if (error)
-                    return;
-
-                me->router_.RouteInbound({std::begin(me->buffer_), std::begin(me->buffer_) + bytesRead});
-                me->Receive();
-            });
-    }
-
-  public:
-    BaseHandler(boost::asio::io_service& service, net::DataRouter const& router)
-      : service_{service}
-      , socket_{service}
-      , router_{router}
-    {
-    }
-
-    boost::asio::ip::tcp::socket& Socket()
-    {
-        return socket_;
-    }
-};
-
-struct ClientHandler : public BaseHandler
-{
-    using Base = BaseHandler;
+    using Base = BaseConnectionHandler;
     ClientHandler(boost::asio::io_service& service, net::DataRouter const& router)
       : Base(service, router)
     {
@@ -69,9 +25,9 @@ struct ClientHandler : public BaseHandler
     }
 };
 
-struct ServerHandler : public BaseHandler
+struct ServerHandler : public BaseConnectionHandler
 {
-    using Base = BaseHandler;
+    using Base = BaseConnectionHandler;
     ServerHandler(boost::asio::io_service& service, net::DataRouter const& router)
       : Base(service, router)
     {
@@ -112,7 +68,7 @@ TEST_CASE("Creating and running services", "[Service]")
     std::string const host = "localhost";
     uint16_t const port = 4094;
 
-    SECTION("")
+    SECTION("Using the services")
     {
         net::Service<ServerHandler> server{net::ServiceMode::Server};
         TestMessageHandler serverMessageHandler{server.GetRouter()};
