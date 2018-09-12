abad6b7f445e5549c98d909a499b338d0bd65457
DennisWG <DennisWG@users.noreply.github.com>
2018-09-12 11:59:17 +0200

    utility/crc32: Added convenience functions 'crc32' and 'validate_crc32' for crc32 hashing

---
+++ src/include/keycap/root/utility/crc32.hpp
@@ -0,0 +1,60 @@
+/*
+    Copyright 2017 KeycapEmu
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include "../types.hpp"
+
+#include <boost/crc.hpp>
+
+namespace keycap::root::network
+{
+    class memory_stream;
+}
+
+namespace keycap::root::utility
+{
+    namespace impl
+    {
+        template <typename T>
+        void hash(boost::crc_32_type& crc, T const& value)
+        {
+            crc.process_bytes(&value, sizeof(T));
+        }
+
+        template <>
+        void hash<network::memory_stream>(boost::crc_32_type& crc, network::memory_stream const& stream);
+
+        template <>
+        void hash<std::string>(boost::crc_32_type& crc, std::string const& str);
+    }
+
+    template <typename... ARGS>
+    uint32 crc32(ARGS&&... args)
+    {
+        thread_local boost::crc_32_type crc;
+
+        (impl::hash(crc, args), ...);
+
+        return crc.checksum();
+    }
+    
+    template <typename... ARGS>
+    bool validate_crc32(uint32 crc, ARGS&&... args)
+    {
+        return crc == crc32(args...);
+    }
+}
---
+++ src/test/utility/crc32.cpp
@@ -0,0 +1,34 @@
+/*
+   Copyright 2017 KeycapEmu
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <keycap/root/utility/crc32.hpp>
+
+#include <rapidcheck/catch.h>
+
+namespace util = keycap::root::utility;
+
+TEST_CASE("crc32")
+{
+    SECTION("Hashing single string must yield a valid result")
+    {
+        REQUIRE(0x4A17B156 == util::crc32(std::string("Hello World")));
+    }
+
+    SECTION("Hashing multiple values must yield a valid result")
+    {
+        REQUIRE(0x4A17B156 == util::crc32(std::string("Hello"), ' ', std::string("World")));
+    }
+}
