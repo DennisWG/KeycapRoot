dc873d0c4f38d98204fe26801805143ade559c9c
DennisWG <DennisWG@users.noreply.github.com>
2019-01-05 07:54:37 +0100

    test appveyor cache

--- build_cache.bat
+++ build_cache.bat
@@ -1,3 +1,8 @@
+IF NOT EXIST build_cache GOTO BUILD_DIR
+GOTO END
+
+:BUILD_DIR
+
 md build_cache
 cd build_cache
 
@@ -9,8 +14,8 @@ md build
 cd build
 cmake -G "Visual Studio 15 2017 Win64" ..
 cmake --build . --config %configuration%
-set ZLIB_LIBRARY="%APPVEYOR_BUILD_FOLDER%\build_cache\zlib-1.2.11\build\%configuration%\zlib.lib"
-set ZLIB_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\zlib-1.2.11"
+
+cd ..\..
 
 appveyor DownloadFile https://botan.randombit.net/releases/Botan-2.8.0.tgz -FileName Botan-2.8.0.tgz
 7z x Botan-2.8.0.tgz > NUL
@@ -20,5 +25,12 @@ configure.py --with-cmake --cpu=%platform%
 cd build
 cmake -G "Visual Studio 15 2017 Win64" ..
 cmake --build . --config %configuration%
+
+:END
+copy zlib-1.2.11\build\Botan-2.8.0 build_cache\Botan-2.8.0
+
+set ZLIB_LIBRARY="%APPVEYOR_BUILD_FOLDER%\build_cache\zlib-1.2.11\build\%configuration%\zlib.lib"
+set ZLIB_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\zlib-1.2.11"
+
 set Botan_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\build_cache\Botan-2.8.0\build\include"
 set Botan_LIBRARIES="%APPVEYOR_BUILD_FOLDER%\build_cache\Botan-2.8.0\build\%configuration%\botan.lib"
