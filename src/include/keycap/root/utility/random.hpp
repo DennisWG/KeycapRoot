0336be950463263482b34047718b8f5fe75b70b5
DennisWG <DennisWG@users.noreply.github.com>
2019-02-02 16:57:01 +0100

    utility/random: Added more random utility functions

--- src/include/keycap/root/utility/random.hpp
+++ src/include/keycap/root/utility/random.hpp
@@ -16,11 +16,20 @@
 
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <limits>
+#include <string>
 
 namespace keycap::root::utility
 {
+    // Returns a random uint8 between min and max (inclusive)
+    // Very fast, not cryptographically safe
+    // Thread safe.
+    uint8_t random_ui8(
+        uint8_t const min = std::numeric_limits<uint8_t>::min(),
+        uint8_t const max = std::numeric_limits<uint8_t>::max());
+
     // Returns a random uint16 between min and max (inclusive)
     // Very fast, not cryptographically safe
     // Thread safe.
@@ -41,6 +50,12 @@ namespace keycap::root::utility
     uint64_t random_ui64(
         uint64_t const min = std::numeric_limits<uint64_t>::min(),
         uint64_t const max = std::numeric_limits<uint64_t>::max());
+
+    // Returns a random int8 between min and max (inclusive)
+    // Very fast, not cryptographically safe
+    // Thread safe.
+    int8_t random_i8(
+        int8_t const min = std::numeric_limits<int8_t>::min(), int8_t const max = std::numeric_limits<int8_t>::max());
 
     // Returns a random int16 between min and max (inclusive)
     // Very fast, not cryptographically safe
@@ -74,4 +89,61 @@ namespace keycap::root::utility
     // Thread safe.
     double random_double(
         double const min = std::numeric_limits<double>::min(), double const max = std::numeric_limits<double>::max());
+
+    // Returns a random char between min and max (inclusive)
+    // Very fast, not cryptographically safe
+    // Thread safe.
+    char
+    random_char(char const min = std::numeric_limits<char>::min(), char const max = std::numeric_limits<char>::max());
+
+    // Returns a random array with values between min and max (inclusive)
+    // Very fast, not cryptographically safe
+    // Thread safe.
+    template <typename T, size_t N>
+    std::array<T, N>
+    random_array(T const min = std::numeric_limits<T>::min(), T const max = std::numeric_limits<T>::max())
+    {
+        std::array<T, N> arr;
+
+        for (size_t i = 0; i < N; ++i)
+        {
+            if constexpr (std::is_same_v<std::remove_cv_t<T>, uint8_t>)
+                arr[i] = random_ui8(min, max);
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, uint16_t>)
+                arr[i] = random_ui16(min, max);
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, uint32_t>)
+                arr[i] = random_ui32(min, max);
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, uint64_t>)
+                arr[i] = random_ui64(min, max);
+
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, int8_t>)
+                arr[i] = random_i8(min, max);
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, int16_t>)
+                arr[i] = random_i16(min, max);
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, int32_t>)
+                arr[i] = random_i32(min, max);
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, int64_t>)
+                arr[i] = random_i64(min, max);
+
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, float>)
+                arr[i] = random_float(min, max);
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, double>)
+                arr[i] = random_double(min, max);
+
+            else if constexpr (std::is_same_v<std::remove_cv_t<T>, char>)
+                arr[i] = random_char(min, max);
+        }
+
+        return arr;
+    }
+
+    namespace impl
+    {
+        std::string const default_chars = "abcdefghijklmnaoqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+
+    // Returns a random string with values with the given length (inclusive) and the given allowed characters
+    // Very fast, not cryptographically safe
+    // Thread safe.
+    std::string random_string(size_t len = 15, std::string const& allowed_chars = impl::default_chars);
 }
--- src/libs/utility/random.cpp
+++ src/libs/utility/random.cpp
@@ -23,6 +23,12 @@ namespace keycap::root::utility
     static thread_local std::random_device random_device;
     static thread_local std::mt19937_64 generator{random_device()};
 
+    uint8_t random_ui8(uint8_t const min, uint8_t const max)
+    {
+        std::uniform_int_distribution<uint16_t> dist(min, max);
+        return static_cast<uint8_t>(dist(generator));
+    }
+
     uint16_t random_ui16(uint16_t const min, uint16_t const max)
     {
         std::uniform_int_distribution<uint16_t> dist(min, max);
@@ -39,6 +45,12 @@ namespace keycap::root::utility
     {
         std::uniform_int_distribution<uint64_t> dist(min, max);
         return dist(generator);
+    }
+
+    int8_t random_i8(int8_t const min, int8_t const max)
+    {
+        std::uniform_int_distribution<int16_t> dist(min, max);
+        return static_cast<int8_t>(dist(generator));
     }
 
     int16_t random_i16(int16_t const min, int16_t const max)
@@ -69,5 +81,20 @@ namespace keycap::root::utility
     {
         std::uniform_real_distribution<double> dist(min, max);
         return dist(generator);
+    }
+
+    char random_char(char const min, char const max)
+    {
+        return static_cast<char>(random_i8(static_cast<int8_t>(min), static_cast<int8_t>(max)));
+    }
+
+    std::string random_string(size_t len, std::string const& allowed_chars)
+    {
+        std::uniform_int_distribution<size_t> dist{0, allowed_chars.length() - 1};
+
+        std::string ret;
+        std::generate_n(std::back_inserter(ret), len, [&] { return allowed_chars[dist(generator)]; });
+
+        return ret;
     }
 }
