d413f8f3d1397a688e3ffd6d2888ceaae1bdc24c
DennisWG <DennisWG@users.noreply.github.com>
2020-03-26 18:42:10 +0100

    update botan libraries

--- build_scripts/build_botan.bat
+++ build_scripts/build_botan.bat
@@ -2,13 +2,13 @@ IF NOT EXIST Botan-2.8.0 GOTO BUILD
 GOTO END
 
 :BUILD
-appveyor DownloadFile https://botan.randombit.net/releases/Botan-2.8.0.tgz -FileName Botan-2.8.0.tgz
+appveyor DownloadFile https://github.com/randombit/botan/archive/2.13.0.zip -FileName Botan-2.8.0.tgz
 7z x Botan-2.8.0.tgz > NUL
 7z x Botan-2.8.0.tar > NUL
 cd Botan-2.8.0
 configure.py --with-cmake --cpu=%platform%
 cd build
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 16 2019" ..
 cmake --build . --config %configuration%
 
 :END
